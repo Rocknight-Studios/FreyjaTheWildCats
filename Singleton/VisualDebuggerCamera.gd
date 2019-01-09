22c239dcb20746f6ca72e93043b0fdbef0e6158c
JanisMancevics <splintercdo@gmail.com>
2019-01-09 18:00:46 +0200

    For insurance.

--- Singleton/VisualDebuggerCamera.gd
+++ Singleton/VisualDebuggerCamera.gd
@@ -3,6 +3,7 @@ extends Camera2D
 var previous_mouse_drag_position = Vector2(.0, .0) # To calculate, where to move.
 
 const ZOOM_COEFFICIENT = .01 # How relatively quickly to zoom in and out.
+const ZOOM_SHIFT_COEFFICIENT = .1 # If shift key is pressed zooming should happen much faster.
 const MOUSE_ZOOM_GODOT_ZOOMOUT_DIRECTION_FIX = 10.0 # To keep the zoom centered.
 const MIN_ZOOM_THRESHOLD = .02 # How close it is allowed to zoom in.
 
@@ -15,7 +16,7 @@ func _input(event):
 		previous_mouse_drag_position = get_viewport().get_mouse_position()
 
 func manage_mouse_zoom_state(direction):
-	var zoom_step = ZOOM_COEFFICIENT * direction # For speed and convenience.
+	var zoom_step = (ZOOM_SHIFT_COEFFICIENT if Input.is_action_pressed("shift_down") else ZOOM_COEFFICIENT) * direction # For speed and convenience.
 	var previous_zoom = zoom # To calculate the relative zoom change.
 	zoom = Vector2(max(zoom.x + zoom_step, MIN_ZOOM_THRESHOLD), max(zoom.y + zoom_step, MIN_ZOOM_THRESHOLD))
 	var half_viewport_size = get_viewport().size * .5 # For speed and convenience.
