703eac06c0160c4addee1f06a941cf5b5e08955a
JanisMancevics <splintercdo@gmail.com>
2019-01-14 07:26:09 +0200

    Outliner node type columns implemented.

--- VisualDebugger/Outliner.gd
+++ VisualDebugger/Outliner.gd
@@ -1,15 +1,17 @@
 extends Tree
 
 onready var show_node_info_button = Global.visual_debugger.get_node("TabContainer/VisualSelect/SelectedNodeInfo/ShowNodeInfoButton") # For speed and convenience.
+onready var indent_width = get_constant("item_margin") # For speed and convenience.
+onready var one_character_width = get_font("font").size * FONT_ASPECT_RATIO # To calculate correctly the column 0 width.
+onready var relative_indent_coefficient = indent_width / one_character_width # What is the factor between ONE_CHARACTER_WIDTH and indent_width.
 
 var tree_item = null # To remember which tree item to manage under which.
 var deepest_branch_width = 0 # To determine how wide should be the h scroll bar.
 var dont_find_the_widest_branch_while_building_the_tree = false # To avoid performing redundant (called by on_collapse signal) work and make management easier.
 var current_deepest_item = null # To widen the scope and make management easier.
 
-const INDENT_WIDTH = 1.0 # How much deeper to push each indent. (Expand triangle characters and icons)
-const SPACE_BETWEEN_COLUMNS = 1.0 # How many characters between columns.
-const ONE_CHARACTER_WIDTH = 10.0 # To calculate correctly the column 0 width.
+const SPACE_BETWEEN_COLUMNS = 5.0 # How many characters between columns.
+const FONT_ASPECT_RATIO = .5 # How much wider is font than it is high.
 
 func add_a_tree_item(node, parent_item):
 	tree_item = create_item(parent_item)
@@ -48,7 +50,7 @@ func find_widest_and_deepest_branches(current_branch_root_item):
 		if str(current_branch_root_item.get_metadata(0)).length() > deepest_branch_width:
 			current_deepest_item = current_branch_root_item
 			var path_name_count = current_branch_root_item.get_metadata(0).get_name_count() # For speed and convenience.
-			deepest_branch_width = (path_name_count - 1) * INDENT_WIDTH + current_branch_root_item.get_metadata(0).get_name(path_name_count - 1).length()
+			deepest_branch_width = path_name_count * relative_indent_coefficient + current_branch_root_item.get_metadata(0).get_name(path_name_count - 1).length()
 			if absolute_widest_branch == null || deepest_branch_width + current_branch_root_item.get_text(1).length() > absolute_widest_branch_width + absolute_widest_branch.get_text(1).length():
 				absolute_widest_branch_width = deepest_branch_width
 				absolute_widest_branch = current_deepest_item
@@ -57,7 +59,8 @@ func find_widest_and_deepest_branches(current_branch_root_item):
 				if current_widest_parent_item == null:
 					break
 				if current_widest_parent_item.collapsed == true:
-					deepest_branch_width = str(current_widest_parent_item.get_metadata(0)).length()
+					path_name_count = current_widest_parent_item.get_metadata(0).get_name_count()
+					deepest_branch_width = path_name_count * relative_indent_coefficient + current_widest_parent_item.get_metadata(0).get_name(path_name_count - 1).length()
 					break
 				current_widest_parent_item = current_widest_parent_item.get_parent()
 		if current_branch_root_item.collapsed != true:
@@ -71,10 +74,8 @@ func _on_Outliner_item_collapsed(item):
 		absolute_widest_branch_width = 0
 		deepest_branch_width = 0
 		find_widest_and_deepest_branches(get_root())
-		print(absolute_widest_branch_width)
-		#set_column_min_width(0, ONE_CHARACTER_WIDTH * (absolute_widest_branch_width + SPACE_BETWEEN_COLUMNS))
-		set_column_min_width(0, ONE_CHARACTER_WIDTH * (absolute_widest_branch_width))
-		absolute_widest_branch_width += absolute_widest_branch.get_text(1).length()
-		print(absolute_widest_branch.get_text(0))
+		absolute_widest_branch_width = (absolute_widest_branch_width + SPACE_BETWEEN_COLUMNS) * one_character_width
 		set_column_expand(1, true)
 		set_column_expand(0, false)
+		set_column_min_width(0, absolute_widest_branch_width)
+		absolute_widest_branch_width += absolute_widest_branch.get_text(1).length() * one_character_width
--- VisualDebugger/OutlinerHScrollBar.gd
+++ VisualDebugger/OutlinerHScrollBar.gd
@@ -3,16 +3,16 @@ extends HScrollBar
 onready var outliner_container = get_parent().get_node("OutlinerContainer") # For speed and convenience.
 onready var outliner = outliner_container.get_node("Outliner") # For speed and convenience.
 onready var outliner_width = outliner.rect_size.x # For speed and convenience.
+onready var branch_width_threshold = outliner.rect_size.x * .42 # If the path to the tree item is wider than this show the horizontal scroll bar.
 
-const BRANCH_WIDTH_THRESHOLD = 80 # If the path to the tree item is wider than this show the horizontal scroll bar.
 const RIGHT_OFFSET_FOR_V_SCROLL_BAR = -12 # To avoid having magic numbers.
 
 func _process(delta):
 	outliner.get_node("@@34").rect_position.x = outliner_container.rect_size.x + RIGHT_OFFSET_FOR_V_SCROLL_BAR + value
-	if outliner.absolute_widest_branch_width > BRANCH_WIDTH_THRESHOLD:
+	if outliner.absolute_widest_branch_width > branch_width_threshold:
 		visible = true
 		max_value = outliner.absolute_widest_branch_width
-		page = outliner.absolute_widest_branch_width - (outliner.absolute_widest_branch_width - BRANCH_WIDTH_THRESHOLD)
+		page = outliner.absolute_widest_branch_width - (outliner.absolute_widest_branch_width - branch_width_threshold)
 		outliner.rect_position.x = -value
 	else:
 		visible = false
